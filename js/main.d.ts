d29516cde25a8cdb7e85e7786a1ba99fe5a91773
Eric Gach <eric.gach@gmail.com>
2025-10-01 16:11:58 -0500

    Updating to fix linting errors

--- js/main.d.ts
+++ js/main.d.ts
@@ -1,5 +1,5 @@
 export function attachMenuToggle(className: string, menuToggleId: string);
-export function bindElements(event: Event);
+export function bindElements();
 export function clearActive(menu_items: HTMLLIElement[], className: string = 'active');
 export function closeMenu(className: string);
 export function highlightNavigationOnScroll(menu_items: HTMLLIElement[], className: string = 'active');
